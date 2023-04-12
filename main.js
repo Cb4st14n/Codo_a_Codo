6a57905a049f206540b0be76e01a2a2d4837ad1e
Cb4st14n <130600079+Cb4st14n@users.noreply.github.com>
2023-04-12 17:50:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	// Slider
+	if(window.location.href.indexOf('index') > -1){
+	  
+	  $('.galeria').bxSlider({
+	    mode: 'fade',
+	    captions: false,
+	    slideWidth: 1450,
+	    responsive: true,
+	    pager: true
+		
+	  });
+
+	}
+});
